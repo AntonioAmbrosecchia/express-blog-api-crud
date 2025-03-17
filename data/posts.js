88d09e340b7ce0683a9d0414c2fd18b0296cb351
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-17 22:41:32 +0100

    add posts

---
+++ data/posts.js
@@ -0,0 +1,7 @@
+const posts = [
+    { id: 1, title: "Primo Post", content: "Questo è il primo post" },
+    { id: 2, title: "Secondo Post", content: "Questo è il secondo post" },
+    { id: 3, title: "Terzo Post", content: "Questo è il terzo post" },
+];
+
+module.exports = posts;
