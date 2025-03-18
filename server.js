c32642b2932dd9202d69d7a63d35aa7386f3b888
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-18 12:09:02 +0100

    add 2nd middleware

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.use((req, res, next) => {
     res.status(404).json({ message: "Endpoint non trovato" })
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({ message: err.message || "Errore interno del server" });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
 });
+
