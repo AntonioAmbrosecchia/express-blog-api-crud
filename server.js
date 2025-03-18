59b0b3ba21bdcae24c459791fab4c9cc9436892d
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-18 11:59:47 +0100

    add middleware

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const postsRoutes = require('./routes/postsRoutes');
 app.use(bodyParser.json());
 app.use('/api', postsRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Endpoint non trovato" })
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
